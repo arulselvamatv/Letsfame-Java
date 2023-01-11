51cc25ed4f6692fce1b2a4e8b7cb618c59527f78
sathish144625 <sathishkumar144625@gmail.com>
2023-01-11 15:08:41 +0530

    WebHooksResponse

---
+++ src/main/java/com/letsfame/bean/WebHooksResponse.java
@@ -0,0 +1,46 @@
+package com.letsfame.bean;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Document(collection = "webhooks")
+public class WebHooksResponse {
+
+	private String id;
+	
+	private int created_at;
+	
+	private int updated_at;
+	
+	private String owner_id;
+	
+	private String owner_type;
+	
+	private String url;
+	
+	private String secret;
+	
+	private String alert_email;
+	
+	private boolean secret_exists;
+	
+	private String entity;
+	
+	private String active;
+	
+	private List events;
+	
+	
+}
--- src/main/java/com/letsfame/service/PlanService.java
+++ src/main/java/com/letsfame/service/PlanService.java
@@ -1,15 +1,12 @@
 package com.letsfame.service;
 
 import java.util.List;
-
 import javax.validation.Valid;
-
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.letsfame.bean.PlanRequest;
 import com.letsfame.repository.PlanRequestReository;
 import com.razorpay.Plan;
